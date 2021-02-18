ed6e4ad21bcaabf4cb4b1d306507a507b70829c5
unknown <eexdhe@gmail.com>
2021-02-18 16:47:20 +0100

    create arrows btn to move snake

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ let snake = null;
 let food = {};
 
 const gameoverScreen = document.querySelector('.gameover');
-const score = document.querySelector('.score')
+const score = document.querySelector('.score');
+const arrows = document.querySelectorAll('.arrow');
+
+arrows.forEach((arrow) => arrow.addEventListener('click', handleClickArrow));
 
 function setup() {
   createCanvas(600, 600);
@@ -42,13 +45,42 @@ function renderFood() {
 function keyPressed() {
   if (keyCode === UP_ARROW) {
     snake.direction(createVector(0, -1));
+    selectArrow('arrow-up');
   } else if (keyCode === DOWN_ARROW) {
     snake.direction(createVector(0, 1));
+    selectArrow('arrow-down');
   } else if (keyCode === RIGHT_ARROW) {
     snake.direction(createVector(1, 0));
+    selectArrow('arrow-right');
   } else if (keyCode === LEFT_ARROW) {
     snake.direction(createVector(-1, 0));
+    selectArrow('arrow-left');
   }
+
+}
+
+function handleClickArrow(e){
+  const arrow = e.target;
+  if(arrow.classList.contains('arrow-up')){
+    snake.direction(createVector(0, -1));
+  }else if(arrow.classList.contains('arrow-down')){
+    snake.direction(createVector(0, 1));
+  }else if(arrow.classList.contains('arrow-right')){
+    snake.direction(createVector(1, 0));
+  }else if(arrow.classList.contains('arrow-left')){
+    snake.direction(createVector(-1, 0));
+  }
+}
+
+function selectArrow(className){
+  arrows.forEach((arrow) => {
+    if(arrow.classList.contains(className)){
+      arrow.classList.add('active');
+      setTimeout(() => {
+        arrow.classList.remove('active');
+      }, 300);
+    }
+  })
 }
 
 const reset = document.querySelector('.reset');
